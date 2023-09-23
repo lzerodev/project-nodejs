f1e13f8c6f271c5602a68c9e0af49f1821998a41
leandro.morais <leandro.morais@fengbrasil.com.br>
2023-09-23 03:31:05 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,60 @@
+
+import { fastify } from 'fastify'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+const database = new DatabasePostgres()
+
+//CREATE
+server.post('/videos', async (request, reply) => {
+  const { title, description, duration } = request.body 
+
+  await database.create({
+    title,
+    description,
+    duration,
+  })
+
+  return reply.status(201).send()
+})
+
+//READ
+
+server.get('/videos', async (request) => {
+  const search = request.query.search
+
+  const videos = await database.list(search)
+
+  return videos
+})
+
+//UPDATE
+
+server.put('/videos/:id', async (request, reply) => {
+  const videoId = request.params.id
+  const { title, description, duration } = request.body 
+
+  await database.update(videoId, {
+    title,
+    description,
+    duration,
+  })
+
+  return reply.status(204).send()
+})
+
+//DELETE
+
+server.delete('/videos/:id', async (request, reply) => {
+ const videoId = request.params.id
+ 
+ await database.delete(videoId)
+
+ return reply.status(204).send()
+})
+
+
+server.listen({
+  port: process.env.PORT ?? 3333,
+})
